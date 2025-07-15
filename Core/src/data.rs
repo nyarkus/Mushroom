5890863083c94252f33e20b68a0bbdab83fc6817
nyarkus <nyarkus@icloud.com>
2025-07-15 18:09:29 +0300

    distance_squared func

--- Core/src/data.rs
+++ Core/src/data.rs
@@ -9,6 +9,12 @@ impl Position {
     pub fn new(x: i32, y: i32) -> Self {
         Self { x, y }
     }
+    pub fn distance_squared(&self, other: &Position) -> i32 {
+        let dx = self.x - other.x;
+        let dy = self.y - other.y;
+
+        dx * dx + dy * dy
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
