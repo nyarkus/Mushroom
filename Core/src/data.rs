6800e134b4273cccdaa77968bab63becba97bcea
nyarkus <nyarkus@icloud.com>
2025-07-15 19:06:47 +0300

    new functions

--- Core/src/data.rs
+++ Core/src/data.rs
@@ -9,11 +9,20 @@ impl Position {
     pub fn new(x: i32, y: i32) -> Self {
         Self { x, y }
     }
-    pub fn distance_squared(&self, other: &Position) -> i32 {
+    pub fn distance_squared(&self, other: Position) -> i32 {
         let dx = self.x - other.x;
         let dy = self.y - other.y;
 
         dx * dx + dy * dy
+    }
+
+    pub fn get_neighbor(&self, dir: Direction) -> Position {
+        match dir {
+            Direction::Up => Position::new(self.x, self.y - 1),
+            Direction::Down => Position::new(self.x, self.y + 1),
+            Direction::Left => Position::new(self.x - 1, self.y),
+            Direction::Right => Position::new(self.x + 1, self.y),
+        }
     }
 }
 
@@ -24,9 +33,22 @@ pub struct Size {
 }
 
 #[derive(Debug, Copy, Clone, EnumIter)]
+#[derive(PartialEq)]
 pub enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
+    Up = 0,
+    Down = 1,
+    Left = 2,
+    Right = 3,
+}
+
+impl Direction {
+    pub fn from(value: i32) -> Direction {
+        match value {
+            0 => Direction::Up,
+            1 => Direction::Down,
+            2 => Direction::Left,
+            3 => Direction::Right,
+            _ => panic!("Invalid direction: {}", value),
+        }
+    }
 }
